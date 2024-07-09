65233db923e6949286b7b036ff568562cb442d18
Александр Рак <alexandr.rak.people@gmail.com>
2024-07-09 16:14:04 +0500

    return createapp

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import "./assets/styles/index.scss";
+import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 import { createPinia } from "pinia";
+import "./assets/styles/index.scss";
 
 const pinia = createPinia();
 const app = createApp(App);
